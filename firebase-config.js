7bc9c0f5e9e560a3e17dc5ab0546a8f4593b71fa
Dancing bull <72798082+upscaspirantmzr@users.noreply.github.com>
2025-08-19 18:56:35 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,22 +3,19 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
-// IMPORTANT: Replace the placeholder below with your actual Firebase project settings.
-// You can find this in your Firebase console under Project settings.
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCzlbVpsLSQECmVUs5wyaXKy-fz8nfhjkM",
-  authDomain: "schooladmsn.firebaseapp.com",
-  projectId: "schooladmsn",
-  storageBucket: "schooladmsn.firebasestorage.app",
-  messagingSenderId: "355479237462",
-  appId: "1:355479237462:web:2f27bad032f6cbde4d5cf7",
-  measurementId: "G-PYGEWXKSH8"
+  apiKey: "AIzaSyC5jrq6D7zEK_KZuKLvbEsg9fOTAejOQKw",
+  authDomain: "myballbnotes.firebaseapp.com",
+  projectId: "myballbnotes",
+  storageBucket: "myballbnotes.firebasestorage.app",
+  messagingSenderId: "310299141154",
+  appId: "1:310299141154:web:86abc79f6468ea55b8567b",
+  measurementId: "G-14MW7L628Y"
 };
-
 // This is the unique identifier for your app's data in Firestore.
 // It's important to use a consistent, unique string here.
-const APP_ID = "1:355479237462:web:2f27bad032f6cbde4d5cf7";
+const APP_ID = ""1:310299141154:web:86abc79f6468ea55b8567b";
 
 let db, auth;
 
